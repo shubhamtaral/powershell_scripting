aacc0954025603b76b8e7d0c3c2b1c1582b1c246
Shubham_Taral <shubham.taral@contractpodai.com>
2021-09-17 18:53:10 +0530

    Added couter to files

--- RenameFiles.ps1
+++ RenameFiles.ps1
@@ -5,28 +5,33 @@ function renameFile {
     )
     
     $pattern = "[#':><?|%/\\]"
-    Write-Host "Creating backup Folder @ $fileFolderPath/_backup ..."
+    $counter = 0;
+    Write-Host "=>INFO: Creating backup Folder @ $fileFolderPath/_backup ..."
     New-Item -Path $fileFolderPath -Name '_backup' -ItemType "directory"
 
     # Get BackUp
+    Write-Host "=>INFO: Taking Backup of Files from $fileFolderPath Which needs a change..."
     $Files = (Get-ChildItem -Exclude _backup, *.xlsx, *.bashrc, *.bash_logout, *.profile, *.docx, *.doc, *.pub, *.pptx, *.zip, *.JEPG $fileFolderPath | 
         ForEach-Object { @{Path = $_.fullname } }).Values
     foreach ($File in $Files) {
         Copy-Item -Path $File -Destination $fileFolderPath/_backup 
         Write-Host "Copied $File to /_backup folder ..."
+        $counter++
     }
     
     # Start Renaming all files (can add -Filter "*.pdf")
-    Write-Host "Renaming Files from $fileFolderPath ..."
+    Write-Host "=>INFO: Renaming Files from $fileFolderPath ..."
     Get-ChildItem -Exclude _backup, *.xlsx, *.bashrc, *.bash_logout, *.profile, *.docx, *.doc, *.pub, *.pptx, *.zip, *.JEPG $fileFolderPath | 
     Rename-Item –NewName { $_.name.trim() –replace $pattern , '_' } -Force
-    Write-Host "Renaming Files Done! ..."
+    Write-Host "=>INFO: Renaming Files Done! ..."
     
     # Adds ".pdf" ext to all files ecluding pdfs
     Write-Host "Adding extention to Files from $fileFolderPath ..."
     Get-ChildItem -Exclude _backup, *.pdf, *.xlsx, *.bashrc, *.bash_logout, *.profile, *.docx, *.doc, *.pub, *.pptx, *.zip, *.JEPG $fileFolderPath | 
     Rename-Item –NewName { $_.name + ".pdf" } -Force
-    Write-Host "Extention Added ...!"
+    Write-Host "=>INFO: Extention Added ...!"
+
+    Write-Host "=>INFO: $counter files Changed...."
 }
 
 # Provide Dir path as input to the function
