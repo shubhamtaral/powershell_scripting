6d3176efc672de41513c274c44016c37eb792a56
Shubham_Taral <shubham.taral@contractpodai.com>
2021-09-30 22:33:35 +0530

    Added SQL Auto Bacpac creation

--- MSSQL_AutoBackups.ps1
+++ MSSQL_AutoBackups.ps1
@@ -1,34 +1,73 @@
-﻿# Import requried modules
-import-module sqlps
+﻿# Function to Perform one time exec for set SQLPackage Path
+function SetSQLPackagePath {
+    # Get the path variables details into the variable 
+    $PathVariables = $env:Path 
+    #Print the path variable
+    $PathVariables 
+ 
+    #Check the path existence of the SqlPackage.exe and print its status 
+    IF (-not $PathVariables.Contains( "C:\Program Files\Microsoft SQL Server\150\DAC\bin")) { 
+        write-host "SQLPackage.exe path is not found, Update the environment variable" 
+        $env:Path = $env:Path + ";C:\Program Files\Microsoft SQL Server\150\DAC\bin;"  
+    } 
+}
 
-$SQLInstance = "DESKTOP-JOJOSJI"
-$DBName = "QA_CL_QA1"
-$SharedFolder = "D:\OFFICE\SQLBackups"
-$Date = Get-Date -format yyyyMMdd
-$BackupFileName = "$DBName-$date.bak"
-$BackupDBName = "$($DBName)_backup)"
-$DataFile = "D:\Databases\NewDB.mdf"
-$LogFile = "D:\Logs\NewDB_log.ldf"
+# Function to Create a BACPAC
+function CreateBACPAC {
+    param (
+        [Parameter(Mandatory = $true)][string] $SourceServerName,
+        [Parameter(Mandatory = $true)][string] $DatabaseName,
+        [Parameter(Mandatory = $true)][string] $DatabaseUserName,
+        [Parameter(Mandatory = $true)][string] $DatabasePasswd,
+        [Parameter(Mandatory = $true)][string] $BackupDirectory,
+        [Parameter(Mandatory = $true)][string] $TargetserverName
+    )
+    
+    # Setting sqlPackage 
+    SetSQLPackagePath
 
-Write-Host "==> INFO: Taking Backup of $DBName..."
-Backup-SqlDatabase  -ServerInstance $SQLInstance `
-    -Database $DBName `
-    -CopyOnly `
-    -CompressionOption off `
-    -BackupFile "$($SharedFolder)\$BackupFileName" `
-    -BackupAction Database `
-    -Verbose
+    # Requried Veriables 
+    $dirName = [io.path]::GetDirectoryName($BackupDirectory) 
+    $filename = $DatabaseName
+    $ext = "bacpac" 
+    $TargetFilePath = "$dirName\$filename-$(get-date -f yyyyMMdd).$ext" 
 
-Write-Host "==> INFO: $DBName backup completed to $SharedFolder\$BackupFileName"
+    Write-Host '==> INFO : Taking BACPAC : ' $TargetFilePath 
+ 
+    # Creating BacPac using SqlPackage
+    SqlPackage.exe /a:Export /ssn:$SourceServerName /sdn:$DatabaseName /su:$DatabaseUserName /sp:$DatabasePasswd /tf:$TargetFilePath 
+ 
+    # Checking if BacPac File Created
+    $NewestBacPacFile = Get-ChildItem -Path $dirName\$filename*.$ext | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+    $file = "$NewestBacPacFile"
+ 
+    Write-Host '==> INFO : BACPAC created : ' $FILE 
 
-Write-Host "==> INFO: Restoring $DBName backup From $SharedFolder\$BackupFileName as $BackupDBName"
+    ZipTheBACPACK "$FILE" "$BackupDirectory" "$DatabaseName"
 
-Restore-SqlDatabase  -ServerInstance $SQLInstance `
-    -Database "$BackupDBName" `
-    -BackupFile "$($SharedFolder)\$BackupFileName" `
-    -verbose
+} 
 
-Write-Host "==> INFO: Restoration of $DBName to $BackupDBName is completed ..."
+# Zip the BACPAC
+function ZipTheBACPACK {
+    param (
+        [Parameter(Mandatory = $true)][string] $FilePath,
+        [Parameter(Mandatory = $true)][string] $destiationPath,
+        [Parameter(Mandatory = $true)][string] $DatabaseName
+    )
+
+    $filename = $DatabaseName
+    $ext = "zip" 
+    $zipname = "$filename-$(get-date -f yyyyMMdd).$ext" 
+    $zippath = Join-Path $destiationPath $zipname
+
+    # Zipping the folder
+    Write-Host "==> INFO : Backing up $FilePath to $zippath"
+    #Compress-Archive -Path $items -DestinationPath $zippath
+    & "C:\Program Files\7-Zip\7z" a $zippath $FilePath
+}
+
+CreateBACPAC 'shr-cp-db-qa.04e49d8760a0.database.windows.net' 'QA_CL_QA1' 'ContractPodAi' 'ContractPod2019!' 'F:\DBBackups' 'DESKTOP-JOJOSJI'
+
 
 # https://docs.microsoft.com/en-us/powershell/module/sqlserver/backup-sqldatabase?view=sqlserver-ps#example-11--backup-a-database-to-the-azure-blob-storage-service
 # https://docs.microsoft.com/en-us/powershell/module/sqlserver/restore-sqldatabase?view=sqlserver-ps#example-8--restore-a-database-from-the-azure-blob-storage-service
