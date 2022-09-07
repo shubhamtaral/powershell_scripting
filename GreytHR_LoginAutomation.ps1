176969f77638b9da22a19a773247197db4d1e8e0
Shubham_Taral <shubham.taral@contractpodai.com>
2022-09-07 11:25:52 +0530

    removed scripts

--- GreytHR_LoginAutomation.ps1
+++ GreytHR_LoginAutomation.ps1
@@ -1,6 +1,6 @@
 $headers = @{
     'Username'= 'IT90'
-    'Password'= 'Staral@1234'
+    'Password'= '#########'
     }
 
 $response = Invoke-RestMethod 'https://contractpod.greythr.com/uas/v1/oauth2/client-token' -Method 'POST' -Headers $headers
