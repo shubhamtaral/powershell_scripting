3e3aa1500e0a908c3194c0a46bd8a3b2a95cc253
Shubham Taral <shubhamtaral007@gmail.com>
2025-02-26 13:04:18 +0530

    Delete GreytHR_LoginAutomation.ps1

--- GreytHR_LoginAutomation.ps1
+++
@@ -1,7 +0,0 @@
-$headers = @{
-    'Username'= 'IT90'
-    'Password'= '#########'
-    }
-
-$response = Invoke-RestMethod 'https://contractpod.greythr.com/uas/v1/oauth2/client-token' -Method 'POST' -Headers $headers
-$response | ConvertTo-Json
